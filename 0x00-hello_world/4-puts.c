c61f8c4097952b9127fa2c31f9ed04f2c852bc0c
tsinatwon <tsinat2wondimu@gmai.com>
2022-03-10 12:51:11 -0800

    first compling

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,12 @@
  *
  * 
  * Return:Always 0 (success)
- * /
- int main(void)
- {
+ **/
+   int main(void)
+  {
  
  
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
-
-}
+	return (0);
+   }
