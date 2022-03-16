2748a112c92c9846170e76376bbecc99e7f3ff53
tsinatwon <99538202+tsinatwon@users.noreply.github.com>
2022-03-16 21:01:40 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,6 +4,9 @@
  * print_sign - print signs for negative and postive numbers
  * @n: input number to process
  *
+ * Return: 1 and print + if n is greater than zero
+ * 0 and 0 if n is zero
+ * -1 and - if n is negative
  */
 int print_sign(int n)
 {
