de0cbadfb1b129a6b64a10ba5fe9a77c285144fe
tsinatwon <99538202+tsinatwon@users.noreply.github.com>
2022-03-16 20:51:19 +0300

    checks sign of given parameter

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - print signs for negative and postive numbers
+ * @n: input number to process
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
