d2f3dba4e2c044d0b8906c9b0a33babf7e56af3d
tsinatwon <99538202+tsinatwon@users.noreply.github.com>
2022-03-18 01:25:10 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,18 @@
 #include "main.h"
+
 /**
  * print_numbers - print all digit
  *
  * Return: void
  */
+
 void print_numbers(void)
 {	
 	int count ;
 
-	for (count = 48 ; count < 58 ;count++)
+	for (count = 48; count < 58; count++)
 	{	
-		_putchar(count)
+		_putchar(count);
 	}
 	_putchar('\n');
 }
