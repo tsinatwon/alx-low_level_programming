e42d3403317998573288e0d679b3e2bb18323638
tsinatwon <99538202+tsinatwon@users.noreply.github.com>
2022-03-17 23:06:30 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,15 @@
 #include "main.h"
-
 /**
-  * main - Test function for positive or negative
-  * Return: 0
-  */
-
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	
-	positive_or_negative(0);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
 }
