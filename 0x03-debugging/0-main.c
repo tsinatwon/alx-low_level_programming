2661887500fa01367889b1cd4b42e04f8853fc3f
tsinatwon <tsinat2wondimu@gmai.com>
2022-03-17 04:49:34 -0700

    checking sign

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main :checks code
+ * Returns :Always zero
+ */
+
+int main(void)
+{	int i;
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,5 @@
+
+#include <stdio.h>
+#define MAIN_H
+#ifndef MAIN_H
+void positive_or_negative(int i);
