decba7d52597ce77b2e1d4de682439bd7f565b25
tsinatwon <tsinat2wondimu@gmai.com>
2022-03-30 13:08:16 -0700

    prints string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints string
- * @s: charcter to be printes
- *
- * Return: void
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
  */
 void _puts_recursion(char *s)
 {
