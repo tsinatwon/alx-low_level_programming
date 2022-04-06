e377c15f6d88f59a07f746fb875c212655ad0197
tsinatwon <tsinat2wondimu@gmail.com>
2022-04-06 21:59:05 +0300

    concat

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,21 +10,24 @@
  */
 char *_strdup(char *str)
 {
-	int strlen = 0, index;
-	char *p;
+	char *duplicate;
+	int index, len = 0;
 
 	if (str == NULL)
 		return (NULL);
-	while (*str != '\0')
-	{
-		strlen++;
-	}
-	strlen++;
-	p = malloc(sizeof(char) * strlen);
 
-	if (p == NULL)
+	for (index = 0; str[index]; index++)
+		len++;
+
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
 		return (NULL);
-	for (index = 0; index <= strlen; strlen++)
-		p[index] = str[index];
-	return (p);
+
+	for (index = 0; str[index]; index++)
+		duplicate[index] = str[index];
+
+	duplicate[len] = '\0';
+
+	return (duplicate);
 }
