425cfce829422502899a341f1bc66d149c233d27
tsinatwon <tsinat2wondimu@gmail.com>
2022-05-05 21:01:42 +0300

    read_txtfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,30 +12,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int num ; 
-    unsigned long  int bytes ;
-    long  int bytes2 ;
-    char *buffer;
-    if (filename == NULL ||  letters == 0)
-        return (0);
-     num = open(filename , O_RDONLY , 0);
-     if (num == -1)
-        return (0);
+	int num;
+	unsigned long  int bytes;
+	long  int bytes2;
+	char *buffer;
 
-    buffer = malloc(sizeof(char) * letters);
-    bytes = read(num , buffer , letters);
+	if (filename == NULL ||  letters == 0)
+		return (0);
+	num = open(filename, O_RDONLY, 0);
+	if (num == -1)
+		return (0);
 
-    if (bytes !=  letters)
-        return (0);
-    buffer[bytes + 1 ] = '\0';
+	buffer = malloc(sizeof(char) * letters);
+	bytes = read(num, buffer, letters);
 
-    bytes2 = write( STDIN_FILENO ,buffer, letters);
+	if (bytes !=  letters)
+		return (0);
+	buffer[bytes + 1] = '\0';
 
-    
-    free(buffer);
-    close(num);
-
-    return (bytes2);
+	bytes2 = write(STDIN_FILENO, buffer, letters);
+	free(buffer);
+	close(num);
+	return (bytes2);
 }
 
 
