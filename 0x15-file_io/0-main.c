cad584912f07939fb1a07764e8f9d2c4c9cf96aa
tsinatwon <tsinat2wondimu@com>
2022-05-05 19:13:12 +0300

    done

---
+++ 0x15-file_io/0-main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+    long int n;
+
+    if (ac != 2)
+    {
+        dprintf(2, "Usage: %s filename\n", av[0]);
+        exit(1);
+    }
+    n = read_textfile(av[1], 114);
+    printf("\n(printed chars: %li)\n", n);
+    n = read_textfile(av[1], 1024);
+    printf("\n(printed chars: %li)\n", n);
+    return (0);
+}
+
--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,53 +1,42 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
 
-ssize_t read_textfile(const char *filename, size_t letters);
+#include "main.h"
 
-
- size_t byte_transfer;
- int num;
-
-
-
-
-int main(int ac, char **av)
-{
-    ssize_t n;
-
-    if (ac != 2)
-    {
-        dprintf(2, "Usage: %s filename\n", av[0]);
-        exit(1);
-    }
-    n = read_textfile(av[1], 114);
-    printf("\n(printed chars: %li)\n", n);
-    n = read_textfile(av[1], 1024);
-    printf("\n(printed chars: %li)\n", n);
-    return (0);
-}
-    
-
-
+/**
+ *read_textfile - reads a text file and prints it to the
+ *POSIX standard output
+ *@filename:pointer to file description
+ *@letters:number of letters it should read and print
+ *Return:actual no. of letters it could read and print
+ */
 
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    size_t byte_transfer;
-    num = open(filename , O_RDWR ,0 );
-    if (num == -1)
-    return 0;
-
-    byte_transfer = read(num ,1 ,  letters);
-
-    if (byte_transfer != letters)
+    int num ; 
+    unsigned long  int bytes ;
+    long  int bytes2 ;
+    char *buffer;
+    if (filename == NULL ||  letters == 0)
         return (0);
-    else 
-        return byte_transfer;
+     num = open(filename , O_RDONLY , 0);
+     if (num == -1)
+        return (0);
 
+    buffer = malloc(sizeof(char) * letters);
+    bytes = read(num , buffer , letters);
 
+    if (bytes !=  letters)
+        return (0);
+    buffer[bytes + 1 ] = '\0';
 
+    bytes2 = write( STDIN_FILENO ,buffer, letters);
 
+    
+    free(buffer);
+    close(num);
 
+    return (bytes2);
 }
+
+
+
